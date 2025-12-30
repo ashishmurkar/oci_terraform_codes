c92f12e899132af2b81eedc7d74d686eac0deb6a
Ashish <ashishmurkar23@gmail.com>
2025-12-30 22:29:56 +0530

    update file

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
   tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaatbnralswjtkdmcrneahdhwiwtvi5h2p7fe6ps4sf57zhpfp55aka"
   user_ocid        = "ocid1.user.oc1..aaaaaaaagfs5buczrug2k6634vfzmw6i4b7ce74vzswqrq4dy6xqkpbpzicq"
-  fingerprint      = "20:3b:97:13:55:1c:4d:49:8e:1f:2a:2b:3c:4d:5e:6f:70"
-  private_key_path = "C:/Users/ashis/OneDrive/Documents/TF/pvt.key"
+  fingerprint      = "ea:64:08:9c:fe:2b:8e:a5:f8:eb:db:60:bf:ec:a2:fa"
+  private_key_path = "C:/Users/ashis/OneDrive/Documents/TF/OCI-Auth-keys/pvt.pem"
   region           = "ap-mumbai-1"
 }
