42fe8359e966cb82448ece34a2d171b44f34d3c5
Ashish <ashishmurkar23@gmail.com>
2026-01-02 10:35:13 +0530

    add route tables and nat gateway

--- internet_gateway.tf
+++ internet_gateway.tf
@@ -9,3 +9,9 @@ resource "oci_core_internet_gateway" "spoke_igw" {
   }
 
 }
+
+resource "oci_core_nat_gateway" "nat_gw" {
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaw7nvo6iigpp447ffwjjga5kfknm3sa6wrgbmhhdkgufsfsob7nua"
+  vcn_id         = "ocid1.vcn.oc1.ap-mumbai-1.amaaaaaagyisp6ia2eqnnpucq35xuvsvd5grch2u5zab7ojztjlz3g5lgoaa"
+  display_name = "flip_nat_gw"
+}
