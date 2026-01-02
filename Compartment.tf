2b5cbb4b3c1466c4109a0261c094c58f895c0276
Ashish <ashishmurkar23@gmail.com>
2026-01-02 09:56:29 +0530

    add internet_gateway.tf, update the vcn.tf

--- Compartment.tf
+++ Compartment.tf
@@ -2,29 +2,29 @@ resource "oci_identity_compartment" "Flip_Compartment" {
   compartment_id = "ocid1.tenancy.oc1..aaaaaaaatbnralswjtkdmcrneahdhwiwtvi5h2p7fe6ps4sf57zhpfp55aka"
   name           = "Flip_Compartment"
   description    = "Compartment for Flip project"
-  
+
   defined_tags = {
     "FLIPKART-TAGS.Environment" = "Prod"
-    }
   }
+}
 
 
 resource "oci_identity_compartment" "Flip_Network_Compartment" {
   compartment_id = "ocid1.compartment.oc1..aaaaaaaagsgye2ft6ssyas7uvsq7ohgehqbo7lstsgvy27chjqpbkuaatl2q"
   name           = "Flip_Network_Comp"
   description    = "Compartment for Flip Network Resources"
-  
+
   defined_tags = {
     "FLIPKART-TAGS.Environment" = "Prod"
-    }
   }
+}
 
 resource "oci_identity_compartment" "Flip_computeandstorage_Compartment" {
   compartment_id = "ocid1.compartment.oc1..aaaaaaaagsgye2ft6ssyas7uvsq7ohgehqbo7lstsgvy27chjqpbkuaatl2q"
   name           = "Flip_computeandstorage_Comp"
   description    = "Compartment for Flip Compute and Storage Resources"
-  
+
   defined_tags = {
     "FLIPKART-TAGS.Environment" = "Prod"
-    }
   }
+}
--- vcn.tf
+++ vcn.tf
@@ -9,14 +9,3 @@ resource "oci_core_vcn" "spoke_vcn" {
   }
 }
 
-resource "oci_core_internet_gateway" "spoke_igw" {
-  compartment_id = "ocid1.compartment.oc1..aaaaaaaaw7nvo6iigpp447ffwjjga5kfknm3sa6wrgbmhhdkgufsfsob7nua"
-  vcn_id         = "ocid1.vcn.oc1.ap-mumbai-1.amaaaaaagyisp6ia2eqnnpucq35xuvsvd5grch2u5zab7ojztjlz3g5lgoaa"
-  display_name   = "flip_igw"
-  enabled     = true
- 
-  defined_tags = {
-        "FLIPKART-TAGS.Environment" = "Prod"
-    }
- 
-}
