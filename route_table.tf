bc44570d7e7efa163725c0ee8067e0a39ca8d05b
Ashish <ashishmurkar23@gmail.com>
2026-01-16 14:44:41 +0530

    import hub_pub_subnet_rt from console and add Hub → Spoke App Subnet via DRG route in route_table.tf

--- route_table.tf
+++ route_table.tf
@@ -14,3 +14,27 @@ resource "oci_core_route_table" "app_rt" {
     "FLIPKART-TAGS.Environment" = "Prod"
   }
 }
+
+resource "oci_core_route_table" "hub_pub_subnet_rt" {
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaks4kmn7u37x36bo2v2fhstp4bftgdjrecinpzvckqnxdkbbe6ubq"
+  vcn_id         = "ocid1.vcn.oc1.ap-mumbai-1.amaaaaaagyisp6iafutoaopywr2lc237xvtdd7a6q5qhce6l6feurxzlkn2a"
+  display_name   = "hub_pub_subnet_RT"
+
+
+# Existing Internet Route
+route_rules {
+  destination       = "0.0.0.0/0"
+  destination_type  = "CIDR_BLOCK"
+  network_entity_id = "ocid1.internetgateway.oc1.ap-mumbai-1.aaaaaaaav7cupdzu2fgvyuux6q3xcawdobi4lc5ovfvxtpavdje7skdzc2sq"
+  description       = "Connectivity to internet"
+}
+
+# Hub → Spoke App Subnet via DRG
+route_rules {
+  destination       = "10.0.2.0/29"
+  destination_type  = "CIDR_BLOCK"
+  network_entity_id = oci_core_drg.hub_to_spoke_drg.id
+  description       = "allowing traffic from hub_pub_subnet to spoke app_sub via DRG"
+}
+
+}
